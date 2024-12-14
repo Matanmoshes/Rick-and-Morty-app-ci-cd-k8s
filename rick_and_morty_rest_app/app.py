ae0d19e0bc4777b3a021c46f25d34cc3903f9f19
Matanmoshes <matanmoshes66@gmail.com>
2024-12-14 13:33:11 +0200

    demo 6

--- rick_and_morty_rest_app/app.py
+++ rick_and_morty_rest_app/app.py
@@ -133,4 +133,4 @@ def handle_internal_error(error):
 
 # Entry point
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    app.run(host='0.0.0.0', port=5010, debug=True)
